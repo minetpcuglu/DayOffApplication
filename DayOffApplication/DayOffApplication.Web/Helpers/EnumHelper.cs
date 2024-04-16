3eed931855c9ef4bb2976e6626681bd588cbc866
SANKOHOLDING\mine.topcuoglu <mine.topcuoglu@sanko.com.tr>
2024-04-16 08:44:43 +0300

    16.04 / Enum description

--- DayOffApplication/DayOffApplication.Web/Helpers/EnumHelper.cs
+++ DayOffApplication/DayOffApplication.Web/Helpers/EnumHelper.cs
@@ -4,6 +4,14 @@ using System.Reflection;
 
 namespace DayOffApplication.Web.Helpers
 {
+    /// <summary>
+    /// GetEnumDataSource metodu, enum'un tüm değerlerini ve bunların metin temsilini içeren bir liste döndürür.
+    /// Value degeri Text kullaının gördüğü alanı
+    /// Static Neden =>  bu sınıfın bir yardımcı işlevi gerçekleştirmesi ve bir örneğe ihtiyaç duyulmamasıdır.
+    /// İşlevselliğine bakıldığında, EnumHelper sınıfının bir durum tutmadığı ve herhangi bir nesneye özgü davranış içermediği görülebilir.
+    /// IConvertible => Çeşitli türlere dönüştürelebilecegini  
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public static class EnumHelper<T> where T : struct, IConvertible
     {
         public static List<object> GetEnumDataSource()
